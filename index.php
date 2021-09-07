3d5a89d6d89176ea9ae5f6ff5b34fdbf3de79e27
Endreo Figueiredo <endreo.cba@gmail.com>
2021-09-07 19:47:45 -0300

    sem coautor

--- index.php
+++ index.php
@@ -267,10 +267,10 @@ try {
 
             <div class="col-lg-6 mb-6">
               <div class="d-flex flex-column flex-sm-row align-items-start">
-                <img src="assets/img/gallery/testimonial-2.png" class="rounded mx-auto d-block col-sm-5 p-1 " alt="...">
+                <img src="assets/img/banner4.png" class="rounded mx-auto d-block col-sm-5 p-1 " alt="...">
                 <img class="me-sm-3 my-2 my-sm-0 align-self-center align-self-sm-start" src="assets/img/icons/quot.svg" alt="..." />
                 <div class="flex-1 text-center text-sm-start">
-                <h6 class="mb-1 fw-bold fs-2">Edifício Três Marias</h6>
+                <h6 class="mb-1 fw-bold fs-2">Edifício Village Tropica</h6>
                 <h6 class="fs-0">  <a  target="_blank" href="https://goo.gl/maps/9bDVMfMPV1cgNpCU8">Localização</a>  </h6>
                 </div>
               </div>
@@ -555,19 +555,6 @@ try {
             </div>
           </div>
              -->
-          <hr class="text-200 mb-0" />
-          <div class="row justify-content-md-between justify-content-evenly py-3">
-            <div class="col-12 col-sm-8 col-md-6 col-lg-auto text-center text-md-start">
-              <p class="fs-0 my-2 text-400">Todos direitos reservados<span class="fw-bold text-500">&copy;Empresa desde, 2015</span></p>
-            </div>
-            <div class="col-12 col-sm-8 col-md-6">
-              <p class="text-center text-md-end text-400"> Feito com &nbsp;
-                <svg class="bi bi-suit-heart-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FD7D72" viewBox="0 0 16 16">
-                  <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"></path>
-                </svg>&nbsp;by&nbsp;<a class="fw-bold text-500" href="https://github.com/endreofig" target="_blank">Endreo Figueiredo</a>
-              </p>
-            </div>
-          </div>
         </div>
      
 
