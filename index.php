adf51c213d0da59e25f450509c991644d27f9496
Endreo Figueiredo <endreo.cba@gmail.com>
2021-10-02 10:45:44 -0300

    atualizando cor

--- index.php
+++ index.php
@@ -192,7 +192,7 @@
             <div class="container">
                 <div class="text-center">
                     <h2 class="section-heading text-uppercase">Safety Sindicos </h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h3 class="section-subheading text-muted"> Seu condomínio em boas mãos.</h3>
                 </div>
                 <ul class="timeline">
                     <li>
