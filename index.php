8fab839a20329a21fdfed6c9f57a56d9beb266c9
Endreo Figueiredo <endreo.cba@gmail.com>
2021-10-02 11:25:54 -0300

    implementação de phpmail para enviode email

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
@@ -322,24 +336,29 @@
                     <h3 class="section-subheading text-muted">O bom síndico se paga, pois ele gera resultados!</h3>
                 </div>
 
-                <form id="contactForm" >
+                <form id="contactForm" method="POST" action="#" >
                     <div class="row align-items-stretch mb-5">
                         <div class="col-md-6">
                             <div class="form-group">
                                 <!-- Name input-->
-                                <input class="form-control" id="name" type="text" placeholder="Nome*" data-sb-validations="required" />
+                                <input class="form-control" id="nome" name="nome" type="text" placeholder="Nome*" data-sb-validations="required" />
                                 <div class="invalid-feedback" data-sb-feedback="name:required">Digite  o nome .</div>
                             </div>
                             <div class="form-group">
                                 <!-- Email address input-->
-                                <input class="form-control" id="email" type="email" placeholder="Your Email *" data-sb-validations="required,email" />
+                                <input class="form-control" id="email" name="email" type="email" placeholder="Email*" data-sb-validations="required,email" />
                                 <div class="invalid-feedback" data-sb-feedback="email:required">Email é obrigatório</div>
                                 <div class="invalid-feedback" data-sb-feedback="email:email">Digite o email</div>
                             </div>
                             <div class="form-group mb-md-0">
                                 <!-- Phone number input-->
-                                <input class="form-control" id="phone" type="tel" placeholder="Telefone*" data-sb-validations="required" />
+                                <input class="form-control" id="phone" name="telefone" type="tel" placeholder="Telefone*" data-sb-validations="required" />
                                 <div class="invalid-feedback" data-sb-feedback="phone:required">Digite o telefone</div>
+                            </div>
+                            <div class="form-group mb-md-0">
+                                <!-- Phone number input-->
+                                <input class="form-control" id="nomecondominio" name="nomecondominio" type="text" placeholder="Condominio*" data-sb-validations="required" />
+                                <div class="invalid-feedback" data-sb-feedback="phone:required">Nome do condominio</div>
                             </div>
                         </div>
                         <div class="col-md-6">
@@ -353,9 +372,88 @@
                     
                    
                     <div class="text-center">
-                        <button class="btn btn-primary btn-xl text-uppercase" id="submitButton" type="submit">Send Message</button>
+                        <button class="btn btn-primary btn-xl text-uppercase" name="enviar" id="submitButton" type="submit">Send Message</button>
                     </div>
                 </form>
+
+
+                <?php
+
+                    //Create an instance; passing `true` enables exceptions
+                    if(isset($_POST['enviar'])){
+
+                    try {
+                        //Server settings
+                    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+                        $mail->isSMTP();                                            //Send using SMTP
+                        $mail->Host       = 'smtp.hostinger.com';                   //Set the SMTP server to send through
+                        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                        $mail->Username   = 'contato@safetysindicos.com.br';        //SMTP username
+                        $mail->Password   = 'caloi157';                             //SMTP password
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+                        //Recipients
+                        $mail->setFrom('contato@safetysindicos.com.br', 'Safety Sindicos Proficionais');
+                        $mail->addAddress($_POST['email'], 'Safety Sindicos Proficionais');     //Add a recipient
+                        //$mail->addAddress('ellen@example.com');               //Name is optional
+                        //$mail->addReplyTo('info@example.com', 'Information');
+                        $mail->addCC('endreo.cba@gmail.com');
+                        $mail->addCC('contato@safetysindicos.com.br');
+                        //$mail->addBCC('bcc@example.com');
+                        $html = '
+                        <!doctype html>
+                        <html lang="pt-br">
+                        <head>
+                        <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+                            <title>Safety Sindicos</title>
+                            <style type="text/css">
+                            .layout{width: 500px;height: 300px;border-radius: 15pt;}
+                            .layout1{width: 500px;height: 150px;border-radius: 15pt;}
+                            .center{text-align: center;}
+                            .orange{background: darkorange;}
+                            </style>
+                        </head> <body><table>
+                            <tr class="layout1"><td> 
+                                </td></tr><tr> <td class="layout orange">
+                                <h1 class="center"> Ola! '.$_POST['nome'].' </h1> <br>
+                                <h3 class="center">Estamos muito felizes em poder atendelo</h3><br>
+                                <h3 class="center">Logo mais entraremos em contato.</h3>
+                                <h3 class="center">
+                                    Foram enviados os seguintes dados: <br>
+                                    Nome:               '.$_POST['nome'].'  <br>
+                                    Email:              '.$_POST['email'].'  <br>
+                                    Telefone:           '.$_POST['telefone'].'  <br>
+                                    Nome do condominio: '.$_POST['nomecondominio'].'  <br>
+                                    Mensagem:           '.$_POST['mensagem'].'  <br>
+                                </h3></td></tr></table></body></html>';
+                        //Attachments
+                        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+                        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+                        //Content
+                        $mail->isHTML(true);                                  //Set email format to HTML
+                        $mail->Subject = 'Gostariamos de ajudar você';
+                        $mail->Body    = $html;
+                        $mail->AltBody = 'Tambem prestamos consultoria para sindicos.';
+
+                        $mail->send();
+                        echo '<br>
+                        <div class="alert alert-success text-center" role="alert">
+                        <br>
+                            SEU EMAIL FOI ENVIADO COM SUCESSO.
+                        <br>
+                        </div>';
+
+                    } catch (Exception $e) {
+                        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+
+                        //teste
+                    }
+
+                    }
+                    ?>
+
             </div>
         </section>
         <!-- Footer-->
